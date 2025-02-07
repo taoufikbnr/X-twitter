e1576001f3ca89212e958715522ee9d88dea358b
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2025-02-07 20:32:54 +0100

    feat(ui):Add Video Upload Section

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -29,16 +29,30 @@ const Share = () => {
         <Image className="object-cover" src="/aot.jpg" alt="aot" fill/>
       </div>
       <div className="flex flex-col gap-4 flex-1">
-        <input className="outline-none border-none bg-transparent placeholder:text-textGray"type="text" placeholder="Write a post" />
+        <input className="outline-none border-none bg-transparent placeholder:text-textGray" type="text" placeholder="Write a post" />
         {/* PREVIEW IMAGE */}
-        {previewURL&& <div className="relative ronded-xl overflow-hidden object-cover">
-          <Image src={previewURL} alt="" width={600} height={600} />
-          <div className="absolute top-1 left-1 bg-borderGray py-1 px-4 rounded-full font-bold cursor-pointer" 
-          onClick={()=>setIsEditorOpen(true)}>
-            Edit
-          </div>
-          </div>
-          }
+          {media?.type.includes("image")&& previewURL&& <div className="relative ronded-xl overflow-hidden object-cover">
+            <Image src={previewURL} alt="" width={600} height={600} />
+              <div className="absolute top-1 left-1 bg-borderGray py-1 px-4 rounded-full font-bold cursor-pointer" 
+              onClick={()=>setIsEditorOpen(true)}>
+                Edit
+              </div>
+              <div className="flex items-center position absolute top-2 right-2 cursor-pointer rounded-full py-2 px-4 bg-borderGray opacity-70 text-white"
+                onClick={()=>setMedia(null)}>
+                  x
+                </div>
+            </div>
+            }
+          {media?.type.includes("video") && previewURL && (
+            <div className="relative ">
+                <video src={previewURL} controls></video>
+                <div className="flex items-center position absolute top-2 right-2 cursor-pointer rounded-full py-2 px-4 bg-borderGray opacity-70 text-white"
+                onClick={()=>setMedia(null)}>
+                  x
+                </div>
+            </div>
+          )}
+          
           {isEditorOpen && previewURL && (
             <ImageEditor
              onClose={()=>setIsEditorOpen(false)}
@@ -47,11 +61,11 @@ const Share = () => {
              setSettings={setSettings}
              />
           )
-
           }
+
         <div className="flex items-center justify-between flex-wrap">
           <div className="flex gap-4 items-center text-iconBlue">
-            <input onChange={handleMediaChange} className="hidden"  accept="image/png,image/jpg" type="file" id="file" />
+            <input onChange={handleMediaChange} className="hidden"  accept="image/*,video/*" type="file" id="file" />
             <label htmlFor="file">
               <PermMedia className="cursor-pointer"/>
             </label>
