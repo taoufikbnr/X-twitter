bad92e1902e579cc0bc44d62267f8a239fb3df0f
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2025-02-13 12:52:14 +0100

    feat(api):fetch user posts

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,13 +1,25 @@
 import { prisma } from "@/prisma"
 import postData from "../utils/postData.json"
 import Post from "./Post"
+import { auth } from "@clerk/nextjs/server"
 
-const Feed = async() => {
-  const data = await prisma.post.findMany()
+const Feed = async({userProfileId}:{userProfileId?:string}) => {
   
+  const {userId} = await auth()
+  if(!userId) return
+  const whereCondition = userProfileId? {parentPostId:null,userId:userProfileId} : {
+    parentPostId:null,
+    userId:{
+    in:[userId,...(await prisma.follow.findMany({where:{followerId:userId},select:{followingId:true}})).map(follow=>follow.followingId)]
+  }}
+
+  
+  const posts = await prisma.post.findMany({where:whereCondition})
+
+
   return (
     <div>
-      {data.map((post,i)=>
+      {posts.map((post,i)=>
         <Post post={post} key={i} type="comment"/>
       )}
     </div>
